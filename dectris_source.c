9e1577d70d46353650259f9c88b37343079f5d85
vitb <vittorio.boccone@dectris.com>
2016-04-14 14:54:51 +0200

    completed header

--- dectris_source.c
+++ dectris_source.c
@@ -11,7 +11,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-void generic_source_data ( int32_t image [])
+void get_data ( int32_t image [])
 {
   for (int i=0; i<=10; i++){ 
     image [i] = 0xdeadf00+i; // 233496320
